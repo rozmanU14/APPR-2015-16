55c54b0902ed68cd2593ea6c6bbfa8d346ef1449
Urška Rožman <rozman.urska@gmail.com>
2015-12-25 23:22:10 +0100

    analiza podatkov

--- analiza.3.faza.r
+++ analiza.3.faza.r
@@ -126,6 +126,96 @@ max(tabela2014$skupni.prirast, na.rm=TRUE)
 tabela2014[tabela2014[["skupni.prirast"]] == "702",] #Ljubljana
 min(tabela2013$skupni.prirast, na.rm=TRUE)
 tabela2013[tabela2013[["skupni.prirast"]] == "-231",] #Maribor
-#Ugotovila sem da je med leto 2010 in 2014 naarvni prirast največji v Ljubljani in najmanjši v Mariboru.
+#Ugotovila sem da je med leto 2010 in 2014 naarvni
+#prirast največji v Ljubljani in najmanjši v Mariboru.
 
 tabela2010[tabela2010$velikost == "pozitiven" , c("kraj")]
+
+#Povprečen naravni prirast po letih:
+#2010 = 17.69
+sum(tabela2010$skupni.prirast,na.rm=TRUE) /211
+#2011 = 15.39
+sum(tabela2011$skupni.prirast,na.rm=TRUE) /211
+#2012 = 12.71
+sum(tabela2012$skupni.prirast,na.rm=TRUE) /211
+#2013 = 8.42
+sum(tabela2013$skupni.prirast,na.rm=TRUE) /211
+#2014 = 10.80
+sum(tabela2014$skupni.prirast,na.rm=TRUE) /211
+#Ugotovila sem, da je naravni prirast po letih od 2010 do 2013 padal,
+#nato se 2014 spet narastel.
+
+#Povprečno število živorojenih moških in žensk po letih ločeno glede na spol.
+#2010:
+sum(tabela2010$zivorojene.zenske,na.rm=TRUE) /211 #51.37
+sum(tabela2010$zivorojeni.moski,na.rm=TRUE) /211 #54.52
+#2011
+sum(tabela2011$zivorojene.zenske,na.rm=TRUE) /211 #50.71
+sum(tabela2011$zivorojeni.moski,na.rm=TRUE) /211 #53.31
+#2012
+sum(tabela2012$zivorojene.zenske,na.rm=TRUE) /211 #50.33
+sum(tabela2012$zivorojeni.moski,na.rm=TRUE) /211 #53.64
+#2013
+sum(tabela2013$zivorojene.zenske,na.rm=TRUE) /211 #48.85
+sum(tabela2013$zivorojeni.moski,na.rm=TRUE) /211 #51.20
+#2014
+sum(tabela2014$zivorojene.zenske,na.rm=TRUE) /211 #48.52
+sum(tabela2014$zivorojeni.moski,na.rm=TRUE) /211 #51.79
+#Ugotovila sem, da poprečnono število živorojenih žensk pada, prav tako tudi 
+#število živorojenih moških. Vsako leto pa je povprečno število živorojenih 
+#moških večje, od števila živorojenih žensk.
+sum(tabela$zivorojene.zenske,na.rm=TRUE) #52701
+sum(tabela$zivorojeni.moski,na.rm=TRUE) #55803
+#v povprečju se na leto rodi več moških kot žensk
+
+#Povprečno število umrlih moških in žensk po letih ločeno glede na spol.
+#2010:
+sum(tabela2010$umrle.zenske,na.rm=TRUE) /211 #44.16
+sum(tabela2010$umrli.moski,na.rm=TRUE) /211 #44.04
+#2011:
+sum(tabela2011$umrle.zenske,na.rm=TRUE) /211 #44.85
+sum(tabela2011$umrli.moski,na.rm=TRUE) /211 #43.77
+#2012:
+sum(tabela2012$umrle.zenske,na.rm=TRUE) /211 #46.66
+sum(tabela2012$umrli.moski,na.rm=TRUE) /211 #44.61
+#2013:
+sum(tabela2013$umrle.zenske,na.rm=TRUE) /211 #46.35
+sum(tabela2013$umrli.moski,na.rm=TRUE) /211 #45.28
+#2014:
+sum(tabela2014$umrle.zenske,na.rm=TRUE) /211 #45.88
+sum(tabela2014$umrli.moski,na.rm=TRUE) /211 #43.63
+#Ugotovila sem, da v povprečju vsako leto umre več žensk kot moških. 
+#Podatki po letih so približno konstantni.
+sum(tabela$umrli.moski,na.rm=TRUE) #46702
+sum(tabela$umrle.zenske,na.rm=TRUE) #48083
+
+#Primerjava Slovenskega prirasta z ostalimi Evropskimi državami:
+#v tabelo podatkiHTML dodamo podatke za Slovenijo.
+rownames(podatkiHTML) <- row.names = novipodatkiDRZAVE
+podatkiHTML["Slovenija",] <- rep(c("1.769","1.539","1.271","0.842"), ncol(podatkiHTML))
+
+#države z največjim in najmanjšim prirastkom:
+#2010:
+max(podatkiHTML$"2010",na.rm=TRUE) #2.19661
+podatkiHTML[podatkiHTML[["2010"]] == "2.19661",] #Islandija
+min(podatkiHTML$"2010",na.rm=TRUE) #1.24982
+podatkiHTML[podatkiHTML[["2010"]] == "1.24982",] #Madžarska
+#2011
+max(podatkiHTML$"2011",na.rm=TRUE) #2.03052
+podatkiHTML[podatkiHTML[["2011"]] == "2.03052",] #Turčija
+min(podatkiHTML$"2010",na.rm=TRUE) #1.23322
+podatkiHTML[podatkiHTML[["2010"]] == "1.23322",] #Madžarska
+#2012
+max(podatkiHTML$"2012",na.rm=TRUE) #2.03943
+podatkiHTML[podatkiHTML[["2011"]] == "2.03943",] #Turčija
+min(podatkiHTML$"2012",na.rm=TRUE) #1.25775
+podatkiHTML[podatkiHTML[["2012"]] == "1.25775",] #San marino
+#2013
+max(podatkiHTML$"2013",na.rm=TRUE) #2.08498
+podatkiHTML[podatkiHTML[["2011"]] == "2.08498",] #Turčija
+min(podatkiHTML$"2013",na.rm=TRUE) #0.842
+podatkiHTML[podatkiHTML[["2013"]] == "0.842",] #Slovenija
+#Ugotovila sem, da je imela največji povp.naravni prirast v letu 2010 Islandija,
+#v letih 2011-2013 pa Turčija"
+#Najmanjši navani prirast izmed Evropskih držav je imela v letih 2010 in 2011 Madžarska, 
+#v letu 2012 San marino, v letu 2013 pa Slovenija.
