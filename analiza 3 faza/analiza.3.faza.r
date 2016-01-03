7d5f88c73685236d62f3421ac0bd664c0468b35b
Urška Rožman <rozman.urska@gmail.com>
2016-01-03 14:31:11 +0100

    dodani grafi

--- analiza 3 faza/analiza.3.faza.r
+++ analiza 3 faza/analiza.3.faza.r
@@ -67,9 +67,6 @@ obc <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.
                        "OB/OB", encoding = "Windows-1250")
 obc <- obc[order(as.character(obc$OB_UIME)),]
 
-
-
-
 obc$PRIRAST2011<-tabela2011$skupni.prirast
 obc$RODNOST2011<-tabela2011$zivorojeni.moski + tabela2011$zivorojene.zenske
 obc$UMRLIVOST2011<-tabela2011$umrli.moski + tabela2011$umrle.zenske
@@ -90,148 +87,180 @@ zem2<-zem1+ geom_point(data = obc , aes(x = Y_C, y = X_C, color = VELIKOST)) +
 
 
 
-ggplot() + geom_polygon(data = obc, aes(x = long, y = lat, group = group, fill = UMRLIVOST2011),color = "grey") +
+zem3<-ggplot() + geom_polygon(data = obc, aes(x = long, y = lat, group = group, fill = UMRLIVOST2011),color = "grey") +
   scale_fill_gradient(low="#fded75", high= "#100f00") +
   guides(fill = guide_colorbar(title = "Umrlivost 2011"))
 #Zemljevid prikazuje umrljivost v letu 2011 po občinah
 
-ggplot() + geom_polygon(data = obc, aes(x = long, y = lat, group = group, fill = RODNOST2011),color = "grey") +
+zem4<-ggplot() + geom_polygon(data = obc, aes(x = long, y = lat, group = group, fill = RODNOST2011),color = "grey") +
   scale_fill_gradient(low="#a65353", high= "#582b2b") +
   guides(fill = guide_colorbar(title = "Rodnost 2011"))
 #Zemljevid prikazuje rodnost v letu 2011 po občinah. 
 
 #Analiza največjega naravnega prirastka po letih:
 #2010:
-filter(tabela2010, skupni.prirast == max(skupni.prirast, na.rm=TRUE))
-max(tabela2010$skupni.prirast, na.rm=TRUE)
-tabela2010[tabela2010[["skupni.prirast"]] == "925",] #Ljubljana
-min(tabela2010$skupni.prirast, na.rm=TRUE)
-tabela2010[tabela2010[["skupni.prirast"]] == "-212",] #Maribor
+filter(tabela2010, skupni.prirast == max(skupni.prirast, na.rm=TRUE)) #Lj
+filter(tabela2010, skupni.prirast == min(skupni.prirast, na.rm=TRUE)) #Mb
 #2011
-max(tabela2011$skupni.prirast, na.rm=TRUE)
-tabela2011[tabela2011[["skupni.prirast"]] == "792",] #Ljubljana 
-min(tabela2011$skupni.prirast, na.rm=TRUE)
-tabela2011[tabela2010[["skupni.prirast"]] == "-220",] #Maribor
+filter(tabela2011, skupni.prirast == max(skupni.prirast, na.rm=TRUE)) #Lj
+filter(tabela2011, skupni.prirast == min(skupni.prirast, na.rm=TRUE)) #Mb
 #2012
-max(tabela2012$skupni.prirast, na.rm=TRUE)
-tabela2012[tabela2012[["skupni.prirast"]] == "812",]  #Ljubljana
-min(tabela2012$skupni.prirast, na.rm=TRUE)
-tabela2012[tabela2012[["skupni.prirast"]] == "-319",] #Maribor
+filter(tabela2012, skupni.prirast == max(skupni.prirast, na.rm=TRUE)) #Lj
+filter(tabela2012, skupni.prirast == min(skupni.prirast, na.rm=TRUE)) #Mb
 #2013
-max(tabela2013$skupni.prirast, na.rm=TRUE)
-tabela2013[tabela2012[["skupni.prirast"]] == "740",] #Ljubljana
-min(tabela2013$skupni.prirast, na.rm=TRUE)
-tabela2013[tabela2013[["skupni.prirast"]] == "-272",] #Maribor
+filter(tabela2013, skupni.prirast == max(skupni.prirast, na.rm=TRUE)) #Lj
+filter(tabela2013, skupni.prirast == min(skupni.prirast, na.rm=TRUE)) #Mb
 #2014
-max(tabela2014$skupni.prirast, na.rm=TRUE)
-tabela2014[tabela2014[["skupni.prirast"]] == "702",] #Ljubljana
-min(tabela2013$skupni.prirast, na.rm=TRUE)
-tabela2013[tabela2013[["skupni.prirast"]] == "-231",] #Maribor
+filter(tabela2014, skupni.prirast == max(skupni.prirast, na.rm=TRUE)) #Lj
+filter(tabela2014, skupni.prirast == min(skupni.prirast, na.rm=TRUE)) #Mb
+
+
 #Ugotovila sem da je med leto 2010 in 2014 naarvni
 #prirast največji v Ljubljani in najmanjši v Mariboru.
 
-tabela2010[tabela2010$velikost == "pozitiven" , c("kraj")]
-
 #Povprečen naravni prirast po letih:
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
 #2010 = 1.532
-sum(tabela2010$skupni.prirast,na.rm=TRUE) / nrow(tabela2010)/10
+povp.pr.2010<-sum(tabela2010$skupni.prirast,na.rm=TRUE) / nrow(tabela2010)/10
 #2011 = 1.539
-sum(tabela2011$skupni.prirast,na.rm=TRUE) /nrow(tabela2011)/10
+povp.pr.2011<-sum(tabela2011$skupni.prirast,na.rm=TRUE) /nrow(tabela2011)/10
 #2012 = 1.264
-sum(tabela2012$skupni.prirast,na.rm=TRUE) /nrow(tabela2012)/10
+povp.pr.2012<- sum(tabela2012$skupni.prirast,na.rm=TRUE) /nrow(tabela2012)/10
 #2013 = 0.838
-sum(tabela2013$skupni.prirast,na.rm=TRUE) /nrow(tabela2013)/10
+povp.pr.2013<-sum(tabela2013$skupni.prirast,na.rm=TRUE) /nrow(tabela2013)/10
 #2014 = 0.842
-sum(tabela2014$skupni.prirast,na.rm=TRUE) /nrow(tabela2014)/10
-=======
-#2010 = 17.69
-sum(tabela2010$skupni.prirast,na.rm=TRUE) /211
-#2011 = 15.39
-sum(tabela2011$skupni.prirast,na.rm=TRUE) /211
-#2012 = 12.71
-sum(tabela2012$skupni.prirast,na.rm=TRUE) /211
-#2013 = 8.42
-sum(tabela2013$skupni.prirast,na.rm=TRUE) /211
-#2014 = 10.80
-sum(tabela2014$skupni.prirast,na.rm=TRUE) /211
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
+povp.pr.2014<-sum(tabela2014$skupni.prirast,na.rm=TRUE) /nrow(tabela2014)/10
+
+#Grafi prikazujejo naravni prirastek občin po letih, črta prikazuje povprečen prirast.
+ggplot(tabela2010) + aes(x = kraj, y = skupni.prirast) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =povp.pr.2010,color="red")
+ggplot(tabela2011) + aes(x = kraj, y = skupni.prirast) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  geom_point() + geom_hline(yintercept =povp.pr.2011,color="blue")
+ggplot(tabela2012) + aes(x = kraj, y = skupni.prirast) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =povp.pr.2012,color="yellow")
+ggplot(tabela2013) + aes(x = kraj, y = skupni.prirast) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =povp.pr.2013,color="orange")
+
 #Ugotovila sem, da je naravni prirast po letih od 2010 do 2013 padal,
 #nato se 2014 spet narastel.
 
 #Povprečno število živorojenih moških in žensk po letih ločeno glede na spol.
 #2010:
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
-z.zenske1010<-sum(tabela2010$zivorojene.zenske,na.rm=TRUE) /211 #51.37
-z.moski.2010<-sum(tabela2010$zivorojeni.moski,na.rm=TRUE) /211 #54.52
+ziv.z.2010<-sum(tabela2010$zivorojene.zenske,na.rm=TRUE) /211 #51.37
+ziv.m.2010<-sum(tabela2010$zivorojeni.moski,na.rm=TRUE) /211 #54.52
+ggplot(tabela2010) + aes(x = kraj, y = zivorojene.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.z.2010,color="orange")
+ggplot(tabela2010) + aes(x = kraj, y = zivorojeni.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.m.2010,color="purple")
+
 #2011
-z.zenske2011<-sum(tabela2011$zivorojene.zenske,na.rm=TRUE) /211 #50.71
-z.moski2011<-sum(tabela2011$zivorojeni.moski,na.rm=TRUE) /211 #53.31
+ziv.z.2011<-sum(tabela2011$zivorojene.zenske,na.rm=TRUE) /211 #50.71
+ziv.m.2011<-sum(tabela2011$zivorojeni.moski,na.rm=TRUE) /211 #53.31
+ggplot(tabela2011) + aes(x = kraj, y = zivorojene.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.z.2011,color="pink")
+ggplot(tabela2011) + aes(x = kraj, y = zivorojeni.moski) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.m.2011,color="red")
+
 #2012
-z.zenske2012<-sum(tabela2012$zivorojene.zenske,na.rm=TRUE) /211 #50.33
-z.moski2012<-sum(tabela2012$zivorojeni.moski,na.rm=TRUE) /211 #53.64
+ziv.z.2012<-sum(tabela2012$zivorojene.zenske,na.rm=TRUE) /211 #50.33
+ziv.m.2012<-sum(tabela2012$zivorojeni.moski,na.rm=TRUE) /211 #53.64
+ggplot(tabela2012) + aes(x = kraj, y = zivorojene.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.z.2012,color="yellow")
+ggplot(tabela2012) + aes(x = kraj, y = zivorojeni.moski) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.m.2012,color="blue")
+
 #2013
-z.zenske2013<-sum(tabela2013$zivorojene.zenske,na.rm=TRUE) /211 #48.85
-z.moski2013<-sum(tabela2013$zivorojeni.moski,na.rm=TRUE) /211 #51.20
+ziv.z.2013<-sum(tabela2013$zivorojene.zenske,na.rm=TRUE) /211 #48.85
+ziv.m.2013<-sum(tabela2013$zivorojeni.moski,na.rm=TRUE) /211 #51.20
+ggplot(tabela2013) + aes(x = kraj, y = zivorojene.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.z.2013,color="green")
+ggplot(tabela2013) + aes(x = kraj, y = zivorojeni.moski) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.m.2013,color="brown")
+
 #2014
-z.zenske2014<-sum(tabela2014$zivorojene.zenske,na.rm=TRUE) /211 #48.52
-z.moski2014<-sum(tabela2014$zivorojeni.moski,na.rm=TRUE) /211 #51.79
-=======
-sum(tabela2010$zivorojene.zenske,na.rm=TRUE) /211 #51.37
-sum(tabela2010$zivorojeni.moski,na.rm=TRUE) /211 #54.52
-#2011
-sum(tabela2011$zivorojene.zenske,na.rm=TRUE) /211 #50.71
-sum(tabela2011$zivorojeni.moski,na.rm=TRUE) /211 #53.31
-#2012
-sum(tabela2012$zivorojene.zenske,na.rm=TRUE) /211 #50.33
-sum(tabela2012$zivorojeni.moski,na.rm=TRUE) /211 #53.64
-#2013
-sum(tabela2013$zivorojene.zenske,na.rm=TRUE) /211 #48.85
-sum(tabela2013$zivorojeni.moski,na.rm=TRUE) /211 #51.20
-#2014
-sum(tabela2014$zivorojene.zenske,na.rm=TRUE) /211 #48.52
-sum(tabela2014$zivorojeni.moski,na.rm=TRUE) /211 #51.79
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
+ziv.z.2014<-sum(tabela2014$zivorojene.zenske,na.rm=TRUE) /211 #48.52
+ziv.m.2014<-sum(tabela2014$zivorojeni.moski,na.rm=TRUE) /211 #51.79
+ggplot(tabela2014) + aes(x = kraj, y = zivorojene.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.z.2014,color="gold")
+ggplot(tabela2014) + aes(x = kraj, y = zivorojeni.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =ziv.m.2014,color="red")
 #Ugotovila sem, da poprečnono število živorojenih žensk pada, prav tako tudi 
 #število živorojenih moških. Vsako leto pa je povprečno število živorojenih 
 #moških večje, od števila živorojenih žensk.
+
 sum(tabela$zivorojene.zenske,na.rm=TRUE) #52701
 sum(tabela$zivorojeni.moski,na.rm=TRUE) #55803
 #v povprečju se na leto rodi več moških kot žensk
 
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
-povprecje.rojstev<-data.frame(z.zenske2010,z.moski2010)
-
-=======
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
 #Povprečno število umrlih moških in žensk po letih ločeno glede na spol.
 #2010:
-sum(tabela2010$umrle.zenske,na.rm=TRUE) /211 #44.16
-sum(tabela2010$umrli.moski,na.rm=TRUE) /211 #44.04
+umr.z.2010<-sum(tabela2010$umrle.zenske,na.rm=TRUE) /211 #44.16
+umr.m.2010<-sum(tabela2010$umrli.moski,na.rm=TRUE) /211 #44.04
+ggplot(tabela2010) + aes(x = kraj, y = umrle.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.z.2010,color="red")
+ggplot(tabela2010) + aes(x = kraj, y = umrli.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.m.2010,color="blue")
+
 #2011:
-sum(tabela2011$umrle.zenske,na.rm=TRUE) /211 #44.85
-sum(tabela2011$umrli.moski,na.rm=TRUE) /211 #43.77
+umr.z.2011<-sum(tabela2011$umrle.zenske,na.rm=TRUE) /211 #44.85
+umr.m.2011<-sum(tabela2011$umrli.moski,na.rm=TRUE) /211 #43.77
+ggplot(tabela2011) + aes(x = kraj, y = umrle.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.z.2011,color="yellow")
+ggplot(tabela2011) + aes(x = kraj, y = umrli.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.m.2011,color="green")
+
 #2012:
-sum(tabela2012$umrle.zenske,na.rm=TRUE) /211 #46.66
-sum(tabela2012$umrli.moski,na.rm=TRUE) /211 #44.61
+umr.z.2012<-sum(tabela2012$umrle.zenske,na.rm=TRUE) /211 #46.66
+umr.m.2012<-sum(tabela2012$umrli.moski,na.rm=TRUE) /211 #44.61
+ggplot(tabela2012) + aes(x = kraj, y = umrle.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.z.2012,color="pink")
+ggplot(tabela2012) + aes(x = kraj, y = umrli.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.m.2012,color="purple")
+
 #2013:
-sum(tabela2013$umrle.zenske,na.rm=TRUE) /211 #46.35
-sum(tabela2013$umrli.moski,na.rm=TRUE) /211 #45.28
+umr.z.2013<-sum(tabela2013$umrle.zenske,na.rm=TRUE) /211 #46.35
+umr.m.2013<-sum(tabela2013$umrli.moski,na.rm=TRUE) /211 #45.28
+ggplot(tabela2013) + aes(x = kraj, y = umrle.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.z.2013,color="red")
+ggplot(tabela2013) + aes(x = kraj, y = umrli.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.m.2013,color="blue")
+
 #2014:
-sum(tabela2014$umrle.zenske,na.rm=TRUE) /211 #45.88
-sum(tabela2014$umrli.moski,na.rm=TRUE) /211 #43.63
+umr.z.2014<-sum(tabela2014$umrle.zenske,na.rm=TRUE) /211 #45.88
+umr.m.2014<-sum(tabela2014$umrli.moski,na.rm=TRUE) /211 #43.63
+ggplot(tabela2014) + aes(x = kraj, y = umrle.zenske) + 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.z.2014,color="yellow")
+ggplot(tabela2014) + aes(x = kraj, y = umrli.moski)+ 
+  geom_point() +theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  geom_point() + geom_hline(yintercept =umr.m.2014,color="green") 
 #Ugotovila sem, da v povprečju vsako leto umre več žensk kot moških. 
 #Podatki po letih so približno konstantni.
 sum(tabela$umrli.moski,na.rm=TRUE) #46702
 sum(tabela$umrle.zenske,na.rm=TRUE) #48083
 
 #Primerjava Slovenskega prirasta z ostalimi Evropskimi državami:
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
-=======
-#v tabelo podatkiHTML dodamo podatke za Slovenijo.
-rownames(podatkiHTML) <- row.names = novipodatkiDRZAVE
-podatkiHTML["Slovenija",] <- rep(c("1.769","1.539","1.271","0.842"), ncol(podatkiHTML))
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
 
 #države z največjim in najmanjšim prirastkom:
 #2010:
@@ -245,18 +274,14 @@ podatkiHTML[podatkiHTML[["2011"]] == "2.03052",] #Turčija
 min(podatkiHTML$"2010",na.rm=TRUE) #1.23322
 podatkiHTML[podatkiHTML[["2010"]] == "1.23322",] #Madžarska
 #2012
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
 max(podatkiHTML$"2012",na.rm=TRUE) #2.08648
 podatkiHTML[podatkiHTML[["2012"]] == "2.08648",] #Turčija
-=======
 max(podatkiHTML$"2012",na.rm=TRUE) #2.03943
 podatkiHTML[podatkiHTML[["2011"]] == "2.03943",] #Turčija
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
 min(podatkiHTML$"2012",na.rm=TRUE) #1.25775
 podatkiHTML[podatkiHTML[["2012"]] == "1.25775",] #San marino
 #2013
 max(podatkiHTML$"2013",na.rm=TRUE) #2.08498
-<<<<<<< HEAD:analiza 3 faza/analiza.3.faza.r
 podatkiHTML[podatkiHTML[["2013"]] == "2.08498",] #Turčija
 min(podatkiHTML$"2013",na.rm=TRUE) #1.20925
 podatkiHTML[podatkiHTML[["2013"]] == "1.20925",] #Portugalska
@@ -276,12 +301,7 @@ sum(podatkiHTML$"2012", na.rm=TRUE) / nrow(podatkiHTML) #1.42
 sum(podatkiHTML$"2013", na.rm=TRUE) / nrow(podatkiHTML) #1.26
 
 #Ugotovila sem, da je povprečni slovenski prirast večji od povprečnega evropskega po letih.
-=======
-podatkiHTML[podatkiHTML[["2011"]] == "2.08498",] #Turčija
-min(podatkiHTML$"2013",na.rm=TRUE) #0.842
-podatkiHTML[podatkiHTML[["2013"]] == "0.842",] #Slovenija
-#Ugotovila sem, da je imela največji povp.naravni prirast v letu 2010 Islandija,
-#v letih 2011-2013 pa Turčija"
-#Najmanjši navani prirast izmed Evropskih držav je imela v letih 2010 in 2011 Madžarska, 
-#v letu 2012 San marino, v letu 2013 pa Slovenija.
->>>>>>> 04f916a684f0e12a8ea67c1e9feedbe52f96849d:analiza.3.faza.r
+
+
+  
+
