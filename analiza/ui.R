59f6c6dbf38dc8db475c8c71965bfdcec43e3f7b
Urška Rožman <rozman.urska@gmail.com>
2016-01-27 21:48:46 +0100

    ..

--- analiza/ui.R
+++ analiza/ui.R
@@ -1,34 +1,19 @@
 library(shiny)
-
-# Rely on the 'WorldPhones' dataset in the datasets
-# package (which generally comes preloaded).
-library(datasets)
-
-# Define the overall UI
-shinyUI(
-  
-  # Use a fluid Bootstrap layout
-  fluidPage(    
-    
-    # Give the page a title
-    titlePanel("Naravni prirast po občinah"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        selectInput("občina", "Občina:", 
-                    choices=krajj),
-        hr(),
-        helpText("Izberi občino")
-      ),
-      
-      # Create a spot for the barplot
-      mainPanel(
-        plotOutput("naravni prirast")  
+  shinyUI(
+    fluidPage(    
+      titlePanel("Naravni prirast po občinah"),
+      sidebarLayout(      
+        sidebarPanel(
+                 selectInput("kraj", "Občina:", 
+                                             choices = levels(tabela$kraj),
+                                            selected = "Ljubljana"),
+          hr(),
+          helpText("Izberi občino")
+        ),
+        mainPanel(
+               plotOutput("pprirast")  
+        )
+        
       )
-      
     )
   )
-)
