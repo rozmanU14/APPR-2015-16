0cf1a78f4cc0f538a1783b9ddaa6ad687118c808
Urška Rožman <rozman.urska@gmail.com>
2016-01-31 20:58:03 +0100

    .

--- analiza/ui.R
+++ analiza/ui.R
@@ -1,19 +1,14 @@
 library(shiny)
-  shinyUI(
-    fluidPage(    
-      titlePanel("Naravni prirast po občinah"),
-      sidebarLayout(      
-        sidebarPanel(
-                 selectInput("kraj", "Občina:", 
-                                             choices = levels(tabela$kraj),
-                                            selected = "Ljubljana"),
-          hr(),
-          helpText("Izberi občino")
-        ),
-        mainPanel(
-               plotOutput("pprirast")  
-        )
-        
-      )
-    )
-  )
+shinyUI(
+  fluidPage(    
+    titlePanel("Naravni prirast po občinah"),
+    sidebarLayout(      
+      sidebarPanel(
+        selectInput("kraj", "Občina:", 
+                    choices = levels(tabela$kraj),
+                    selected = "Ljubljana"),
+        hr(),
+        helpText("Izberi občino")
+      ),
+      mainPanel(
+        plotOutput("pprirast")))))
