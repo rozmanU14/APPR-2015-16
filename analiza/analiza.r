69d4ff76c3e1653b863c8fcb475ed02d082e9ffc
Urška Rožman <rozman.urska@gmail.com>
2016-01-31 20:35:01 +0100

    uvoženi novi podatki

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,2 +1,31 @@
 library(MASS)
 library(shiny)
+
+
+kolonaLj<-c("leto","prirast")
+uuvozi<-function(){
+  return(read.csv2(file="podatki/ljubljana.csv",
+                   col.names=kolonaLj,
+                   header=FALSE,
+                   na.strings = "-",
+                   fileEncoding = "UTF-8",
+                   as.is = FALSE))
+}
+tabelaLjubljana<-uuvozi()
+tabelaLjubljana$prirast <- as.numeric(tabelaLjubljana$prirast)
+
+
+kolonaSlo<-c("leto","rodnost na 1000","umrlivost na 1000")
+uuvozi<-function(){
+  return(read.csv2(file="podatki/rodnost;umrlivost(slo).csv",
+                   col.names=kolonaSlo,
+                   header=FALSE,
+                   na.strings = "-",
+                   fileEncoding = "UTF-8",
+                   as.is = FALSE))
+}
+tabelaSlo<-uuvozi()
+tabelaSlo$rodnost.na.1000 <- as.numeric(tabelaSlo$rodnost.na.1000)
+tabelaSlo$umrlivost.na.1000 <- as.numeric(tabelaSlo$umrlivost.na.1000)
+
+
